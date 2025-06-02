00b9f38a36c1f43d5b8e1eeb478c4ff12be601e9
mt <mandytang2025@gmail.com>
2025-06-02 11:08:35 +0800

    step 8 : about page

--- pages/views.py
+++ pages/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from django.urls import reverse
 from django.http import HttpResponse
+from listings.models import Listing
+from realtors.models import Realtor
 
 
 # Create your views here.
 
 def index(request):
-    #pages_url = reverse('pages:index')
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_publish=True)[:3]
+    context = {"listings" : listings}
+    return render(request, 'pages/index.html', context)
+
 
 def about(request):
-    #pages_url = reverse('pages:about')
-    return render(request, 'pages/about.html')
+    realtors_base = Realtor.objects.all()
+    realtors = realtors_base.order_by('-hire_date')
+    mvp_realtors = realtors_base.filter(is_mvp=True)
+    context = {"realtors" : realtors, "mvp_realtors" : mvp_realtors}
+    return render(request,'pages/about.html', context)
 
 
