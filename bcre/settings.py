ea340b5ca56d849df99eead01bdfeb13fd016158
mt <mandytang2025@gmail.com>
2025-06-02 16:16:32 +0800

    step 9 : listing page

--- bcre/settings.py
+++ bcre/settings.py
@@ -138,6 +138,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'bcre/static')
 ]
+#STATICFILES_STORAGE = 'whitenoise,storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
